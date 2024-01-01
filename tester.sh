552fbf591b0405ab8641016958b9407df64480f7
Shalyca Sottoriva <ssottori@c1r2s7.42london.com>
2024-01-01 17:41:55 +0000

    partitions

--- tester.sh
+++ tester.sh
@@ -6,42 +6,82 @@ RED='\033[0;31m'
 VIOLET='\033[95m'
 GREEN='\033[1;92m'
 GRAY='\033[0;90m'
+CYAN='\033[0;96m'
 
 USER=$(whoami)
+
+echo -e "${CYAN}      ____                  ___   ____       ____              __ ${NC}\n"
+echo -e "${CYAN}     / __ \____  _________ |__ \ / __ \___  / __ \____  ____  / /_${NC}\n"
+echo -e "${BLUE}    / __  / __ \/ ___/ __ \__/ // __  / _ \/ /_/ / __ \/ __ \/ __/${NC}\n"
+echo -e "${BLUE}   / /_/ / /_/ / /  / / / / __// /_/ /  __/ _, _/ /_/ / /_/ / /_  ${NC}\n"
+echo -e "${VIOLET}/_____/\____/_/  /_/ /_/____/_____/\___/_/ |_|\____/\____/\__/  by pandashaly${NC}\n"
+                                                                
 
 # if [ $USER != "root" ];then
 #	echo "${RED}Opps! You don't have permission. Make sure you run the command with sudo permission - (sudo bash tester.sh)${RESET}\n\n";
 #	exit;
 #fi
 
-echo -e "${VIOLET}=^..^=   =^..^=   =^..^= ABOUT YOUR VM  =^..^=    =^..^=    =^..^=${NC}\n\n"
+echo -e "${BLUE}=^..^=   =^..^=   =^..^= ABOUT YOUR VM  =^..^=    =^..^=    =^..^=${NC}\n\n"
 
 echo -e "${VIOLET}What is a VM and what are the benefits of using VMs?\n${NC}"
 echo -e "${VIOLET}Why did you choose this OS?\n${NC}"
 echo -e "${VIOLET}What's the difference between the two OS's?\n${NC}"
 echo -e "${VIOLET}What's the difference between 'aptitude' and 'apt'?${NC}\n"
 
-echo -e "${GRAY}=^..^=   =^..^=   =^..^= SIMPLE CONFIG  =^..^=    =^..^=    =^..^=${NC}\n"
+echo -e "${BLUE}=^..^=   =^..^=   =^..^= SIMPLE CONFIG  =^..^=    =^..^=    =^..^=${NC}\n"
 echo -e "${GRAY}==================================================================${NC}\n"
 
 echo -e "${VIOLET}       GUI MODE CHECK:        ${NC}\n"
 RES=$(ls /usr/bin/*session)
-if [[ $RES == "usr/bin/dbus-run-session" ]];then
+if [[ $RES == "usr/bin/dbus-run-session" ]]; then
 	echo -e "${GREEN}YAY! GUI mode is disabled! OK ✔${NC}\n"
 else
 	echo -e "${RED}UH-OHH! GUI mode is enabled! KO ✗${NC}\n"
 fi
 
+echo -e "${GRAY}==================================================================${NC}\n"
+
 echo -e "${VIOLET}       PASSWORD CHECK & HOSTNAME:        ${NC}\n"
 sudo chage -l $USER
 
-echo "\n"
+echo -e "${GRAY}==================================================================${NC}\n"
+
 echo -e "${VIOLET}       VM SETUP && PARTITIONS:        ${NC}\n"
-echo "\n"
 echo -e "${VIOLET}Chosen OS - Debian or Rocky:${NC}\n"
-lsb_release -a || cat /etc/os-release
-echo "\n"
+# lsb_release -a || cat /etc/os-release
 hostnamectl
+
+echo -e "${VIOLET}Partitions Check:${NC}\n"
+
+RES=$(lsblk | grep lvm | wc -l)
+if [ $RES -gt 1 ];then
+  echo -e "${GREEN}[OK] ✔${GRAY} lvm${NC}\n"
+else
+	echo -e "${RED}[KO] ✗${GRAY} no lvm partition${NC}\n"
+fi
+RES=$(lsblk | grep home | wc -l)
+if [ $RES -gt 0 ];then
+	echo -e "${GREEN}[OK] ✔${GRAY} home${NC}\n"
+else
+	echo -e "${RED}[KO] ✗${GRAY} no home partition${NC}\n"
+fi
+RES=$(lsblk | grep swap | wc -l)
+if [ $RES -gt 0 ];then
+	echo -e "${GREEN}[OK] ✔${GRAY} swap${NC}\n"
+else
+	echo -e "${RED}[KO] ✗${GRAY} no swap partition${NC}\n"
+fi
+RES=$(lsblk | grep root | wc -l)
+if [ $RES -gt 0 ];then
+	echo -e "${GREEN}[OK] ✔${GRAY} root${NC}\n"
+else
+	echo -e "${RED}[KO] ✗${GRAY} no root partition${NC}\n"
+fi
+
+echo -e "${VIOLET}Bonus Partitions:${NC}\n"
+
+lsblk
 
 echo -e "${GRAY}==================================================================${NC}\n"
 
