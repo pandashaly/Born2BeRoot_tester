52d806879860a341447cfa06274b60a5de59f20f
pandashaly <shalycatori@gmail.com>
2024-01-02 00:54:03 +0000

    added comments

--- tester.sh
+++ tester.sh
@@ -25,9 +25,9 @@ echo -e "${CYAN}/_____/\____/_/  /_/ /_/____/_____/\___/_/ |_|\____/\____/\__/  
 echo -e "${CYAN}=^..^=   =^..^=   =^..^= ABOUT YOUR VM  =^..^=    =^..^=    =^..^=${NC}\n"
 
 echo -e "${VIOLET}What is a VM and what are the benefits of using VMs?\n${NC}"
-echo -e "${VIOLET}Why did you choose this OS?\n${NC}"
-echo -e "${VIOLET}What's the difference between the two OS's?\n${NC}"
-echo -e "${VIOLET}What's the difference between 'aptitude' and 'apt'?${NC}\n"
+echo -e "${VIOLET}Which OS did you chose and why?\n${NC}"
+echo -e "${VIOLET}What's the difference between Debian and Rocky?\n${NC}"
+echo -e "${VIOLET}If you chose Debian, what's the difference between 'aptitude' and 'apt'?\nIf you chose Rocky, what are SELinux and DNF?${NC}\n"
 
 echo -e "${CYAN}=^..^=   =^..^=   =^..^=  =^..^=  =^..^=  =^..^=    =^..^=    =^..^=${NC}\n"
 echo -e "${GRAY}==================================================================${NC}\n"
@@ -43,7 +43,14 @@ fi
 echo -e "${GRAY}==================================================================${NC}\n"
 
 echo -e "${VIOLET}PASSWORD CHECK & HOSTNAME:        ${NC}\n"
+# A password will be requested before any connection attempt on this machine. Log in with a non-root user??. Verify that the chosen password respects the rules imposed in the subject. (password criteria)
 sudo chage -l $USER
+
+# First, create a new user. Assign them a password of your choice, respecting the subject's rules. The evaluated person must now explain how they were able to implement the requested rules on their virtual machine. Normally, one or two files would have been modified.
+
+# Now that you have a new user, ask the evaluated person to create a group named "evaluating" in front of you and assign it to this user. Finally, check that this user indeed belongs to the "evaluating" group.
+
+# what is the purpose of this password policy, and advantages or disadvantages of it.
 
 echo -e "${GRAY}==================================================================${NC}\n"
 
@@ -52,7 +59,14 @@ echo -e "${VIOLET}Chosen OS - Debian or Rocky:${NC}\n"
 # lsb_release -a || cat /etc/os-release
 hostnamectl
 
+# Change this hostname by replacing the login with yours, then restart the machine. If upon restarting, the hostname has not been updated, the evaluation stops here.
+# You can now restore the original hostname of the machine
+
 echo -e "${VIOLET}Partitions Check:${NC}\n"
+
+# This part is an opportunity to discuss partitions! The evaluated person must give you a brief explanation of how LVM works and why it's beneficial.
+
+# What are partitions and how does LVM work
 
 RES=$(lsblk | grep lvm | wc -l)
 if [ $RES -gt 1 ];then
@@ -109,6 +123,12 @@ fi
 lsblk
 
 echo -e "${GRAY}==================================================================${NC}\n"
+
+# SUDO
+
+# Verify that the "sudo" program is indeed installed on the virtual machine.</li> The evaluated person must now show how to assign your new user to the "sudo" group.</li>
+# The subject imposes strict rules for sudo. The evaluated person must first explain the purpose and operation of sudo with examples of their choice. Then, they must demonstrate the implementation of the rules imposed by the subject.</li>
+# Verify that the "/var/log/sudo/" directory exists and contains at least one file. Check the contents of the files in this directory; you should see a history of commands used with sudo. Finally, attempt to run a command via sudo. Check if the file(s) in the "/var/log/sudo/" directory have been updated. If something does not work as expected or is not clearly explained, the evaluation stops here.
 
 echo -e "${VIOLET}UFW Check:${NC}\n"
 
@@ -175,10 +195,22 @@ sudo service ssh status
 echo -e "${GRAY}==================================${NC}\n"
 # Display SSH Configuration
 echo -e "${GRAY}SSH Config:${NC}\n"
-cat /etc/ssh/sshd_config # | grep -E '^#?PermitRootLogin'  # Display the PermitRootLogin line
+cat /etc/ssh/sshd_config | grep -E '^#?PermitRootLogin'  # Display the PermitRootLogin line
 
 echo -e "${GRAY}=====================================================================${NC}\n"
 # group_name ="evaluating"
 # user_name ="user42"
 
 # sudo user
+
+# CHRON - Script monitoring
+# The operation of their script by displaying its code
+# What is cron
+
+# How they have set up their script to execute every 10 minutes from server launch. Once the proper functioning of the script is verified, the evaluated person must make sure that this script executes every minute. You can launch whatever you wish to ensure that the script runs correctly with dynamic values. Finally, the evaluated person must ensure that the script no longer runs at server launch, without modifying the script itself. To verify this point, the server will need to be restarted one last time. At startup, it must be verified that the script still exists in the same place, that its permissions have remained unchanged, and that it has not been modified. If any of the above points are not correct, the evaluation stops here.
+
+# BONUS 
+# Partitions
+# Wordpress site
+# The setup of WordPress, with only the services listed in the subject, will be worth 2 points</li>
+# The free-choice service will be worth 1 point. Verify and test the proper functioning and implementation of each additional service. For the free-choice bonus service, the evaluated person must provide a simple explanation of the service and explain the reasons for their choice. Be aware: NGINX and Apache2 are forbidden.
